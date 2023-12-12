538a84230c4f3b44adc173ba7d1eb6ea725c75b8
Shakil Khan <shakilkhan116797@gmail.com>
2023-12-12 21:51:43 +0600

    Update Trapezoidal method.cpp

--- Trapezoidal method.cpp
+++ Trapezoidal method.cpp
@@ -4,12 +4,9 @@ using namespace std;
 double fx(double x)
 {
     return 1/(1+x*x);
-
 }
 int main ()
 {
-
-
     double lower,upper,step,integration;
     cin>>lower>>upper>>step;
     double h=(upper-lower)/step;
@@ -20,7 +17,6 @@ int main ()
     {
         int x= lower+i*h;
         integration = integration + fx(x);
-
     }
     integration =integration*h;
     cout<<integration;
